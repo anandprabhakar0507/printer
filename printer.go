af5e22456aba69c80317b5b3c74f22d1a26b0b9d
m4ll0k <m4ll0k@protonmail.com>
2018-07-27 11:34:03 +0200

    Update printer.go

--- printer.go
+++ printer.go
@@ -9,74 +9,77 @@ package printer
 import "fmt"
 
 // colors
-var black  = 30
-var red    = 31
-var green  = 32
-var yellow = 33
-var blue   = 34
-var purple = 35
-var cyan   = 36
-var gray   = 37
-var white  = 38
+var BLACK   = "\033[1;30m"
+var LBLACK  = "\033[0;30m"
+var RED     = "\033[1;31m"
+var LRED    = "\033[0;31m"
+var GREEN   = "\033[1;32m"
+var LGREEN  = "\033[0;32m"
+var YELLOW  = "\033[1;33m"
+var LYELLOW = "\033[0;33m"
+var BLUE    = "\033[1;34m"
+var LBLUE   = "\033[0;34m"
+var PURPLE  = "\033[1;35m"
+var LPURPLE = "\033[0;35m"
+var CYAN    = "\033[1;36m"
+var LCYAN   = "\033[0;36m"
+var GRAY    = "\033[1;37m"
+var LGRAY   = "\033[0;37m"
+var WHITE   = "\033[1;38m"
+var LWHITE  = "\033[0;38m"
+var RESET   = "\033[0m"
 // labels
-var info = "[i]"
-var plus = "[+]"
-var less = "[-]"
-var test = "[*]"
-var warn = "[!]"
-var ask  = "[?]"
-
-
-// [i] Hello Word
-func Info(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[1;%dm%s\033[0m \033[1;%dm%s\033[0m\n",yellow,info,yellow,str)
-	} else {
-		fmt.Printf("\033[1;%dm%s\033[0m %s\n",yellow,info,str)
+var INFO = "[i]"
+var PLUS = "[+]"
+var LESS = "[-]"
+var TEST = "[*]"
+var WARN = "[!]"
+var ASK  = "[?]"
+//
+func Info(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",YELLOW,INFO,RESET,YELLOW,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",YELLOW,INFO,RESET,str)
 	}
 }
-
-// [+] Hello Word
-func Plus(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[1;%dm%s\033[0m \033[1;%dm%s\033[0m\n",green,plus,green,str)
-	} else {
-		fmt.Printf("\033[1;%dm%s\033[0m %s\n",green,plus,str)
+//
+func Plus(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",GREEN,PLUS,RESET,GREEN,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",GREEN,PLUS,RESET,str)
 	}
 }
-
-// [-] Hello Word
-func Less(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[0;%dm%s\033[0m \033[0;%dm%s\033[0m\n",red,less,red,str)
-	} else {
-		fmt.Printf("\033[0;%dm%s\033[0m %s\n",red,less,str)
+//
+func Less(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",LRED,LESS,RESET,LRED,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",LRED,LESS,RESET,str)
 	}
 }
-
-// [*] Hello Word
-func Test(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[1;%dm%s\033[0m \033[1;%dm%s\033[0m\n",blue,test,blue,str)
-	} else {
-		fmt.Printf("\033[1;%dm%s\033[0m %s\n",blue,test,str)
+//
+func Test(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",BLUE,TEST,RESET,BLUE,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",BLUE,TEST,RESET,str)
 	}
 }
-
-// [!] Hello Word
-func Warn(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[1;%dm%s\033[0m \033[1;%dm%s\033[0m\n",red,warn,red,str)
-	} else {
-		fmt.Printf("\033[1;%dm%s\033[0m %s\n",red,warn,str)
+//
+func Warn(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",RED,WARN,RESET,RED,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",RED,WARN,RESET,str)
 	}
 }
-
-// [?] Hello Word
-func Ask(str string,colorable bool) {
-	if colorable {
-		fmt.Printf("\033[1;%dm%s\033[0m \033[1;%dm%s\033[0m\n",cyan,ask,cyan,str)
-	} else {
-		fmt.Printf("\033[1;%dm%s\033[0m %s\n",cyan,ask,str)
+//
+func Ask(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",CYAN,ASK,RESET,CYAN,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",CYAN,ASK,RESET,str)
 	}
 }
